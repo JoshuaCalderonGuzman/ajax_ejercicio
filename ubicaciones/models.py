58eb31e01686d99cfc2a3728f35daecbadd201b2
Joshua Calderon Guzman <s22120150@alumnos.itsur.edu.mx>
2025-10-15 19:30:07 -0600

    Creamos los modelos

--- ubicaciones/models.py
+++ ubicaciones/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=100)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
